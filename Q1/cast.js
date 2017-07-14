20cdb84b3f6711d7cbe8d0a4a3b576e40d89a7fd
Aaron Goodman <Amgoodman5@Aarons-MacBook-Air.local>
2017-07-14 14:23:41 -0600

    refactored

--- Q1/cast.js
+++ Q1/cast.js
@@ -28,7 +28,6 @@ function $select(){
 }
 
 
-
 function $parallax(){
   var aarolax =  $('.parallax').parallax();
   return aarolax
@@ -45,29 +44,42 @@ function $parallax(){
         }
     }
 
+            function $appendCategoryToForm(category) {
+               var categories = category.podcasts;
+                for (var i = 0; i < categories.length; i++) {
+                    var option = '<option value="' +
+                        categories[i].podcasts_url + '" > ' +
+                        categories[i].title + '</option>';
+                        // $getPodCast(category.podcasts[i].podcasts_url)
+                   $('select').append(option)
+                }
+            }
+
+
     function $returnSelected() {
         $('select').change(function() {
             var selected = $(this).val();
-                $getPodCast(selected)
+            console.log(selected)
+              $getPodCast(selected)
+
               $('.present').html('');
               $('.present').append('<h4>' + `HERE THEY ARE! `  + '</h4>')
     })
 }
 
 
-function $getPodCast(url) {
+    function $getPodCast(url) {
   $('.show').html('');
-  var button = `<a href =` +  `"http://www.stitcher.com/stitcher-list/all-podcasts-top-shows"`
-  + `id = "download-button" class="btn-large waves-effect waves-light teal lighten-1" ` + ` >FIND & LISTEN</a>`;
-   var card =  `<a href =` +  `"http://www.stitcher.com/stitcher-list/all-podcasts-top-shows">`+ `<div class = "col s4 m4">`
-   + `<div class="card card-panel hoverable ">` +   `<div class="card-image">` +
-   image + `<div class="card-action">` + title + button + `</a>` ;
         var url = "https://galvanize-cors.herokuapp.com/https://feedwrangler.net/" + url;
         $.get(url, function(data) {
             for (var i = 0; i < 3; i++) {
               var image = `<img src = `  + data.podcasts[i].image_url +  ` class= "responsive" >`;
               var title =  `<p class ="title">` + data.podcasts[i].title + `</p>`;
-
+               var button = `<a href =` +  `"http://www.stitcher.com/stitcher-list/all-podcasts-top-shows"`
+               + `id = "download-button" class="btn-large waves-effect waves-light teal lighten-1" ` + ` >FIND & LISTEN</a>`;
+                var card =  `<a href =` +  `"http://www.stitcher.com/stitcher-list/all-podcasts-top-shows">`+ `<div class = "col s4 m4">`
+                + `<div class="card card-panel hoverable ">` +   `<div class="card-image">` +
+                image + `<div class="card-action">` + title + button + `</a>` ;
 
                 $('.show').append(card);
 
