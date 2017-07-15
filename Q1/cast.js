1b5e2aee174c436c51a870620b0138e18c014b1c
Aaron Goodman <Amgoodman5@Aarons-MacBook-Air.local>
2017-07-15 16:02:11 -0600

    refactored

--- Q1/cast.js
+++ Q1/cast.js
@@ -1,91 +1,91 @@
 $(document).ready(function() {
 
-
-var api = "https://galvanize-cors.herokuapp.com/https://feedwrangler.net/api/v2/podcasts/categories?client_key=b5d42c0169eda03e135efd59042b79d2"
-
-$.get(api, function(category) {
-  $materialize(),
-  $parallax(),
-  $appendCategoryToForm(category),
-  $returnSelected(),
-  $select()
-
-    //nav ///
-function $materialize(){
-   var collapseButton = $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
-  });
- return collapseButton
-}
+    var api = "https://galvanize-cors.herokuapp.com/https://feedwrangler.net/api/v2/podcasts/categories?client_key=b5d42c0169eda03e135efd59042b79d2"
+    var image = " ";
+    var title = " ";
+    var button= " ";
+    var card = " ";
+    var option = " ";
+    var category;
+    var aarolax;
+    var collapseButton;
 
 
-function $select(){
- var selectOption = $('select').material_select();
- return selectOption;
-}
 
+    $.get(api, function(category) {
+            $materialize(),
+            $parallax(),
+            $appendCategoryToForm(category),
+            $returnSelected(),
+            $select(),
+            $button()
 
-function $parallax(){
-  var aarolax =  $('.parallax').parallax();
-  return aarolax
-}
-
-    function $appendCategoryToForm(category) {
-       var categories = category.podcasts;
-        for (var i = 0; i < categories.length; i++) {
-            var option = '<option value="' +
-                categories[i].podcasts_url + '" > ' +
-                categories[i].title + '</option>';
-                // $getPodCast(category.podcasts[i].podcasts_url)
-           $('select').append(option)
+        //nav ///
+        function $materialize() {
+                collapseButton = $('.button-collapse').sideNav({
+                menuWidth: 300, // Default is 240
+                edge: 'right', // Choose the horizontal origin
+                closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                draggable: true // Choose whether you can drag to open on touch screens
+            });
+            return collapseButton
         }
-    }
 
-            function $appendCategoryToForm(category) {
-               var categories = category.podcasts;
-                for (var i = 0; i < categories.length; i++) {
-                    var option = '<option value="' +
-                        categories[i].podcasts_url + '" > ' +
-                        categories[i].title + '</option>';
-                        // $getPodCast(category.podcasts[i].podcasts_url)
-                   $('select').append(option)
+
+        function $select() {
+            var selectOption = $('select').material_select();
+            return selectOption;
+        }
+
+
+        function $parallax() {
+           aarolax = $('.parallax').parallax();
+            return aarolax
+        }
+
+
+
+        function $appendCategoryToForm(category) {
+            var categories = category.podcasts;
+            for (var i = 0; i < categories.length; i++) {
+                category = categories[i]
+                 option = '<option value="' +
+                  category.podcasts_url + '" > ' +
+                  category.title + '</option>';
+                // $getPodCast(category.podcasts[i].podcasts_url)
+                $('select').append(option)
+            }
+        }
+
+
+        function $returnSelected() {
+            $('select').change(function() {
+                var selected = $(this).val();
+                $getPodCast(selected)
+                $('.present').html('');
+                $('.present').append('<h4>' + `HERE THEY ARE! ` + '</h4>')
+            })
+        }
+
+
+        function $getPodCast(url) {
+            $('.show').html('');
+            var url = "https://galvanize-cors.herokuapp.com/https://feedwrangler.net/" + url;
+            $.get(url, function(data) {
+                for (var i = 0; i < 3; i++) {
+                     image = `<img src = ` + data.podcasts[i].image_url + ` class= "responsive" >`;
+                    title = `<p class ="title">` + data.podcasts[i].title + `</p>`;
+                    button = `<a href =` + `"http://www.stitcher.com/stitcher-list/all-podcasts-top-shows"` +
+                        `id = "download-button" class="btn-large waves-effect waves-light teal lighten-1" ` + ` >FIND & LISTEN</a>`;
+                     card = `<a href =` + `"http://www.stitcher.com/stitcher-list/all-podcasts-top-shows">` + `<div class = "col s4 m4">` +
+                        `<div class="card card-panel hoverable ">` + `<div class="card-image">` +
+                        image + `<div class="card-action">` + title + button + `</a>`;
+
+                    $('.show').append(card);
+
                 }
-            }
 
-
-    function $returnSelected() {
-        $('select').change(function() {
-            var selected = $(this).val();
-            console.log(selected)
-              $getPodCast(selected)
-
-              $('.present').html('');
-              $('.present').append('<h4>' + `HERE THEY ARE! `  + '</h4>')
+            })
+        }
     })
-}
-
-
-    function $getPodCast(url) {
-  $('.show').html('');
-        var url = "https://galvanize-cors.herokuapp.com/https://feedwrangler.net/" + url;
-        $.get(url, function(data) {
-            for (var i = 0; i < 3; i++) {
-              var image = `<img src = `  + data.podcasts[i].image_url +  ` class= "responsive" >`;
-              var title =  `<p class ="title">` + data.podcasts[i].title + `</p>`;
-               var button = `<a href =` +  `"http://www.stitcher.com/stitcher-list/all-podcasts-top-shows"`
-               + `id = "download-button" class="btn-large waves-effect waves-light teal lighten-1" ` + ` >FIND & LISTEN</a>`;
-                var card =  `<a href =` +  `"http://www.stitcher.com/stitcher-list/all-podcasts-top-shows">`+ `<div class = "col s4 m4">`
-                + `<div class="card card-panel hoverable ">` +   `<div class="card-image">` +
-                image + `<div class="card-action">` + title + button + `</a>` ;
-
-                $('.show').append(card);
-
-            }
-
-        })
-    }
-})
 });
